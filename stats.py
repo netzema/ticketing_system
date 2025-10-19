6c04e5a1869fdf6202b982239173f63232ec602c
Daniel Netzl <netzl.daniel@gmail.com>
2025-10-19 11:23:44 +0000

    distribution of entries + less waiting time

--- stats.py
+++ stats.py
@@ -25,3 +25,43 @@ max_arrival = df["scanned_at"].max()
 print(f"{avg_arrival=}")
 print(f"{min_arrival=}")
 print(f"{max_arrival=}")
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+
+pdf = df.to_pandas()
+pdf["scanned_at_hr"] = pdf["scanned_at"].dt.floor("h")
+pdf["scanned_at_min"] = pdf["scanned_at"].dt.floor("5min")
+scans_per_minute = pdf.groupby("scanned_at_min").size()
+scans_per_hour = pdf.groupby("scanned_at_hr").size()
+
+plt.figure(figsize=(12, 6))
+scans_per_minute.plot(kind="bar", width=1.0, color="skyblue")
+
+plt.title("Verteilung der Eintrittszeiten")
+plt.xlabel("Zeitpunkt (auf Minute gerundet)")
+plt.ylabel("Anzahl gescannter Tickets")
+plt.xticks(rotation=90)
+plt.tight_layout()
+
+ax = plt.gca()
+PLOT_PATH = f"{BASE_PATH}/entry_distribution_minute.png"
+plt.savefig(PLOT_PATH, dpi=300)
+plt.close()
+
+plt.figure(figsize=(12, 6))
+scans_per_hour.plot(kind="bar", width=1.0, color="skyblue")
+
+plt.title("Verteilung der Eintrittszeiten")
+plt.xlabel("Zeitpunkt (auf Stunde gerundet)")
+plt.ylabel("Anzahl gescannter Tickets")
+plt.xticks(rotation=90)
+plt.tight_layout()
+
+ax = plt.gca()
+PLOT_PATH = f"{BASE_PATH}/entry_distribution_hour.png"
+plt.savefig(PLOT_PATH, dpi=300)
+plt.close()
+
+print(f"Plots saved to {PLOT_PATH}")
