df4500df2b8f98c190568a7fd6d719dabaf5ae8b
netzema <daniel@netzl.com>
2026-04-04 22:33:31 +0200

    small updates

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # event config
-EVENT = "oktoberfest2025_prd"
-N_TICKETS = 1800
+EVENT = "oktoberfest2026_prd"
+N_TICKETS = 1500
 BASE_PATH = f"events/{EVENT}"
 BACKUP_ROOT = "bkp"
 
 # netork config
-HOST = "138.199.223.81" # Hetzner Server
+HOST = "91.98.87.80" # Hetzner Server
 # HOST = "192.168.1.4" # Internet home
 # HOST = "172.20.10.8:8000" # iPhone Hotspot
 PORT = "8000"
-URL = "tickets.danielnetzl.com" # without www.
+URL = "jugend.netzldatasolutions.at" # without www.
 # URL = HOST + ":" + PORT # Hosted locally
 
 # page size (in mm)
